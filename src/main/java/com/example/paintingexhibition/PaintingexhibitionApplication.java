12c9503fde4f2b8ad7d06f3f57146c646c339c26
bleta12 <102874311+bleta12@users.noreply.github.com>
2025-05-22 11:24:22 +0200

    Initial commit for painting exhibition Spring project

---
+++ src/main/java/com/example/paintingexhibition/PaintingexhibitionApplication.java
@@ -0,0 +1,13 @@
+package com.example.paintingexhibition;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PaintingexhibitionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PaintingexhibitionApplication.class, args);
+	}
+
+}
